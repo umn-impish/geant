5ce751d624b7a8be3da3eb5cd6baa962c822e6a6
William Setterberg <sette095@umn.edu>
2024-11-13 13:38:53 -0600

    collimator test: extra helpful info

--- import-capable/scripts/collimator_test_macro.py
+++ import-capable/scripts/collimator_test_macro.py
@@ -1,27 +1,30 @@
 import astropy.units as u
 import numpy as np
 
-start = '''
+# keV
+emin, emax = 10, 200
+
+start = f'''
 /control/execute macros/collimator.mac
 
 # Flat energy distribution
 /gps/ene/type Lin
-/gps/ene/min 15 keV
-/gps/ene/max 400 keV
+/gps/ene/min {emin} keV
+/gps/ene/max {emax} keV
 
 # Zero slope, constant intercept --> flat across E
 /gps/ene/gradient 0
 /gps/ene/intercept 1
 
 # Turn off visualization if in interactive mode
-# /vis/disable
+/vis/disable
 
 '''
 
 macro_fn = 'collimator_diagnose.mac'
 
 # the angles we wanna test
-d_theta = 0.4
+d_theta = 0.25
 th0, th1 = 0, 70
 angles = np.arange(th0, th1+d_theta, d_theta) << u.deg
 
@@ -37,5 +40,5 @@ with open('collimation-test.mac', 'w') as f:
         )
         print(f'/run/beamOn {num_runs}', file=f)
         print('', file=f)
-    print('# angle info:\n#', th0, th1, d_theta, file=f)
-
+    print('# angle info (t0, t1 inclusive, dt):\n#', th0, th1, d_theta, file=f)
+    print('# energy info (min, max):\n', emin, emax, file=f)
