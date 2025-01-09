edf5e73bdca8d2a10ec50c187bf38c6c6f2b66b5
William Setterberg <william.s.setterberg@gmail.com>
2025-01-09 00:58:19 -0600

    touch-ups for 1mm thick optical pad
    will need to modify for thinner one

--- import-capable/scripts/construct_trapezoid.py
+++ import-capable/scripts/construct_trapezoid.py
@@ -15,84 +15,86 @@ vertices = (
     (side, 0)
 )
 
-crystal = (
-cq.Workplane("XY")
-  .polyline(vertices)
-  .close()
-  .extrude(thick/2, both=True)
-  .faces(">Y")
-  .workplane(offset=-thick/2)
-  .transformed(rotate=(45, 0, 0))
-  .split(keepTop=False, keepBottom=True)
-)
+if __name__ == '__main__':
+    crystal = (
+    cq.Workplane("XY")
+      .polyline(vertices)
+      .close()
+      .extrude(thick/2, both=True)
+      .faces(">Y")
+      .workplane(offset=-thick/2)
+      .transformed(rotate=(45, 0, 0))
+      .split(keepTop=False, keepBottom=True)
+    )
 
 # Now we can do the same thing to make a nice shell
-air_gap = 10/1000
-esr_thick = 80/1000
-delta = air_gap # + esr_thick
-shell_vertices = (
-    (0, 0),
-    (0, side + delta),
-    (2 * (side + delta), side + delta),
-    (side + delta, 0)
-)
+    air_gap = 10/1000
+    esr_thick = 80/1000
+    delta = air_gap # + esr_thick
+    shell_vertices = (
+        (0, 0),
+        (0, side + delta),
+        (2 * (side + delta), side + delta),
+        (side + delta, 0)
+    )
 
 # We need to translate the shell to exactly encapsulate
 # the crystal
-shell_vertices = tuple(
-    (x - delta, y - delta)
-    for (x, y) in shell_vertices
-)
+    shell_vertices = tuple(
+        (x - delta, y - delta)
+        for (x, y) in shell_vertices
+    )
 
 
 # Compensate for air gap
-shell_thick = thick + 2*delta
+    shell_thick = thick + 2*delta
 
-shell = (
-cq.Workplane("XY")
-  .polyline(shell_vertices)
-  .close()
-  .extrude(shell_thick/2, both=True)
-  .faces(">Y")
-  .workplane(offset=-shell_thick/2)
-  .transformed(rotate=(45, 0, 0))
-  .split(keepTop=False, keepBottom=True)
-  .faces(">Y")
-  .shell(esr_thick, kind='intersection')
-)
+    shell = (
+    cq.Workplane("XY")
+      .polyline(shell_vertices)
+      .close()
+      .extrude(shell_thick/2, both=True)
+      .faces(">Y")
+      .workplane(offset=-shell_thick/2)
+      .transformed(rotate=(45, 0, 0))
+      .split(keepTop=False, keepBottom=True)
+      .faces(">Y")
+      .shell(esr_thick, kind='intersection')
+    )
 
 # Now build up the optical pad
-pad_height, pad_length, pad_depth = 6, 68, 0.2
-optical_pad = (
-cq.Workplane("XZ")
-  .transformed(rotate=(-45, 0, 0))
-  .box(pad_length, pad_height, pad_depth)
-  # Move the pad to the edge, and then move it back a little
-  # to put it on the crystal face
-  .translate((
-      # Move to the center of the crystal
-      side,
-      (optical_pad_center := side + pad_depth/np.sqrt(2) - thick/2),
-      0
-  ))
-  # Center it slightly on the crystal
-  .translate((0, -pad_depth/2, -pad_depth/2))
-)
+    pad_height, pad_length= 6, 68
+    pad_depth = 1
+    optical_pad = (
+    cq.Workplane("XZ")
+      .transformed(rotate=(-45, 0, 0))
+      .box(pad_length, pad_height, pad_depth)
+      # Move the pad to the edge, and then move it back a little
+      # to put it on the crystal face
+      .translate((
+          # Move to the center of the crystal
+          side,
+          (optical_pad_center := side + pad_depth/np.sqrt(2) - thick/2),
+          0
+      ))
+      # Center it slightly on the crystal
+      .translate((0, -0.25, -0.25))
+    )
 
 # Finally, we can put the SiPMs
-sipm_delta = 0.5
-sipm_height, sipm_length, sipm_depth = 7, 70, 1
-sipms = (
-cq.Workplane("XZ")
-  .transformed(rotate=(-45, 0, 0))
-  .box(sipm_length, sipm_height, sipm_depth)
-  .translate((side, optical_pad_center + pad_depth/np.sqrt(2) + sipm_depth/np.sqrt(2), 0))
-  # Translate to roughly center the crystal on the SiPMs
-  .translate((0, -sipm_delta, -sipm_delta))
-)
+    sipm_delta = 1
+    sipm_height, sipm_length, sipm_depth = 7, 70, 1
+    sipms = (
+    cq.Workplane("XZ")
+      .transformed(rotate=(-45, 0, 0))
+      .box(sipm_length, sipm_height, sipm_depth)
+      .translate((side, optical_pad_center + pad_depth/np.sqrt(2) + sipm_depth/np.sqrt(2), 0))
+      # Translate to roughly center the crystal on the SiPMs
+      .translate((0, -sipm_delta, -sipm_delta))
+    )
 
-os.makedirs('trapezoid', exist_ok=True)
-cq.exporters.export(crystal, 'trapezoid/crystal.stl', opt=dict(ascii=True))
-cq.exporters.export(shell, 'trapezoid/esr_shell.stl', opt=dict(ascii=True))
-cq.exporters.export(optical_pad, 'trapezoid/opticalpad.stl', opt=dict(ascii=True))
-cq.exporters.export(sipms, 'trapezoid/sipms.stl', opt=dict(ascii=True))
+    os.makedirs('trapezoid', exist_ok=True)
+    cq.exporters.export(crystal, 'trapezoid/crystal.stl', opt=dict(ascii=True))
+    cq.exporters.export(shell, 'trapezoid/esr_shell.stl', opt=dict(ascii=True))
+    cq.exporters.export(optical_pad, 'trapezoid/opticalpad.stl', opt=dict(ascii=True))
+    cq.exporters.export(sipms, 'trapezoid/sipms.stl', opt=dict(ascii=True))
--- import-capable/scripts/make_trapezoid_macro.py
+++ import-capable/scripts/make_trapezoid_macro.py
@@ -17,8 +17,8 @@ and launch photons from the grid points.
 '''
 
 import numpy as np
+from construct_trapezoid import thick, side
 
-side = 33
 # mm from edge of the crystal we wanna be
 padding = 0.2
 
@@ -26,7 +26,7 @@ num_y = 10
 num_x = 10
 num_runs_per_pt = 1000
 
-thickness = 4
+thickness = thick
 num_z = 5
 
 # The edge of the crystal is a shifted y = x line
@@ -37,19 +37,18 @@ y_edge = lambda z: side - thickness + z
 if __name__ == '__main__':
     with open('trapezoid/optical.mac', 'w') as f, open('trapezoid/metadata.txt', 'w') as mf:
         f.write(
-    '''
+'''
 # Set up the G4 GPS to fire monoenergetic opticals
 # from a variety of points, isotropic across solid angle
-    /control/execute macros/optical-diagnose.mac
-    '''
+/control/execute macros/optical-diagnose.mac
+'''
         )
         for z in np.linspace(-thickness/2 + padding, thickness/2 - padding, num_z):
             for y in np.linspace(padding, y_edge(z) - padding, num_y):
                 for x in np.linspace(padding, x_edge(y) - padding, num_x):
                     f.write(
-    f'''
-    /gps/pos/centre {x:.4f} {y:.4f} {z:.4f} mm
-    /run/beamOn {num_runs_per_pt}
-
-    ''')
+f'''
+/gps/pos/centre {x:.4f} {y:.4f} {z:.4f} mm
+/run/beamOn {num_runs_per_pt}
+''')
                     print(x, y, z, file=mf)
