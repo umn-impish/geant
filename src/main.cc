3327e3181107792c8d11ac9df8c021e7b1cd20c7
William Setterberg <william.s.setterberg@gmail.com>
2026-08-26 16:51:54 -0500

    swap physics to ftfp_bert

--- src/main.cc
+++ src/main.cc
@@ -47,7 +47,7 @@ int main(int argc, char* argv[]) {
     );
 
     G4PhysListFactory plf;
-    auto* pl = plf.GetReferencePhysList("QGSP_BIC_HP_EMZ");
+    auto* pl = plf.GetReferencePhysList("FTFP_BERT_EMZ");
     pl->RegisterPhysics(new G4OpticalPhysics);
     pl->RegisterPhysics(new G4RadioactiveDecayPhysics);
     runManager->SetUserInitialization(pl);
